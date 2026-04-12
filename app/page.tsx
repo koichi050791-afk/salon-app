f0a3b756b999d03b0e39243b25973aa5c233d05f
Koichi <koichi.050791@gmail.com>
2026-04-12 20:32:28 +0900

    fix home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>サロンKPIアプリ</h1>
-
-      <a href="/store/1/weekly">
-        週次入力ページへ
-      </a>
+    <div>
+      <h1>OK</h1>
     </div>
   );
 }
